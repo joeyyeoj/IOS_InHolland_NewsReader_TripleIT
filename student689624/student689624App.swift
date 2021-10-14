a519c8aa5d34ad5d629918c0b5b2d526e6a188c9
Joey <joeypeschier@gmail.com>
2021-10-14 11:28:07 +0200

    Initial Commit

---
+++ student689624/student689624App.swift
@@ -0,0 +1,17 @@
+//
+//  student689624App.swift
+//  student689624
+//
+//  Created by user206790 on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct student689624App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
